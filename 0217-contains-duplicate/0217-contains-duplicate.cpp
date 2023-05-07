ab6b9bf24b47d7e7336bb3fccbe1a5bde7d89101
navneet5252 <111011596+navneet5252@users.noreply.github.com>
2023-05-08 02:08:45 +0530

    Time: 163 ms (32.92%), Space: 71.6 MB (21.97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-      map<int,int>mpp;
-        for(auto it:nums){
-           mpp[it]++;
-        }
-        for(auto it:mpp){
-            if(it.second>1){
-                return true;
-            }
+        unordered_set<int>st;
+      for(auto it:nums){
+         st.insert(it);
+      }
+        if(nums.size()>st.size()){
+            return true;
         }
         return false;
     }
