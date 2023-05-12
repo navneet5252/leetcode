9fc7d6f322671f8fa7b44a2e7c20c8f916a55af7
navneet5252 <111011596+navneet5252@users.noreply.github.com>
2023-05-13 00:24:37 +0530

    Time: 4 ms (9.60%), Space: 6 MB (54.80%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int _gcd(int divisor ,int divident){
+        while(divident%divisor !=0){
+            int rem = divident%divisor;
+            divident = divisor;
+            divisor = rem;
+        }
+        return divisor;
+    }
+    int smallestEvenMultiple(int n) {
+        if(n==1){
+          return 2;
+        }
+        int gcd = _gcd(2,n);
+        int lcm = (n*2)/gcd;
+        return lcm;
+    }
+    
+};
