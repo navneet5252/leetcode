fb4fca8c823570ec8e79f7394c0afbaf5c2b872b
navneet5252 <111011596+navneet5252@users.noreply.github.com>
2023-05-20 18:32:22 +0530

    Time: 9 ms (6.07%), Space: 7.2 MB (16.04%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    void solve(string &s, string part){
+         if(s.find(part)==string::npos){
+           return;
+         }
+        
+        if(s.find(part)!=string::npos){
+            s.erase(s.find(part),part.length());
+        }
+        solve(s,part);
+    }
+public:
+    string removeOccurrences(string s, string part) {
+      // string ans="";
+      solve(s,part);
+      return s;
+    }
+};
