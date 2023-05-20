10649f6acb11555935862ca22a748cbfcd87f6bc
navneet5252 <111011596+navneet5252@users.noreply.github.com>
2023-05-20 17:16:00 +0530

    Time: 77 ms (5.15%), Space: 180.5 MB (6.13%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -9,66 +9,32 @@ class Solution {
           
           rev(s,start,end);
       }
+    void add(string num1,int i ,string num2,int j,int carry,string &ans){
+         //base case
+          if(i<0 &&j<0){
+              if(carry!=0){
+                  ans.push_back(carry+'0');
+              }
+              return;
+          }
+        
+        int val1 = (i>=0? num1[i]:'0') - '0';
+        int val2 = (j>=0?num2[j]:'0')-'0';
+        int sum = val1+val2+carry;
+        char digit = sum%10 + '0';
+        carry = sum/10;
+        ans.push_back(digit);
+         add(num1,i-1,num2,j-1,carry,ans);
+        
+    }
 public:
     string addStrings(string num1, string num2) {
-         string ans="";
-        int i = num1.length()-1;
-        int j = num2.length()-1;
-        int carry =0;
-        while(i>=0 && j>=0){
-            int val1 = num1[i] -'0';
-            int val2 = num2[j] -'0';
-            int sum = val1 + val2 + carry;
-            if(sum<10){
-                char ch = sum + '0';
-                ans.push_back(ch);
-                carry=0;
-                
-            }
-            else{
-                 char ch = sum%10 + '0';
-                ans.push_back(ch);
-                carry = sum/10;
-            }
-            i--,j--;
-        }
-        while(i>=0){
-          
-                 int sum =( num1[i--] - '0')+carry;
-                   if(sum<10){
-                   char ch = sum + '0';
-                   ans.push_back(ch);
-                   carry=0;
-                  }
-                
-            
-            else{
-                 char ch = sum%10 + '0';
-                ans.push_back(ch);
-                carry = sum/10;
-               }
-            
-        }
-        while(j>=0){
-            int sum =( num2[j--] - '0')+carry;
-                   if(sum<10){
-                   char ch = sum + '0';
-                   ans.push_back(ch);
-                   carry=0;
-                  }
-                
-            
-            else{
-                 char ch = sum%10 + '0';
-                ans.push_back(ch);
-                carry = sum/10;
-                 }
-        }
-        if(carry>0){
-             int ch = carry+'0';
-             ans.push_back(ch);
-        }
+        int i=num1.length()-1, j =num2.length()-1;
+        int carry=0;
+        string ans="";
+        add(num1,i ,num2, j,carry,ans);
         rev(ans,0,ans.length()-1);
         return ans;
+
     }
 };
